7f96f51e5fc9703c53fb5acc5442e9c46ea41231
KARNATI SAI VENKAT <90895995+saivenkat735@users.noreply.github.com>
2024-02-16 12:11:02 +0530

    Add files via upload

---
+++ Valid_Anagram.py
@@ -0,0 +1,9 @@
+def isAnagram(s,t):
+    if len(s)!=len(t):
+        return false
+    countS,countT={},{}
+    for i in range(len(s)):
+        countS[s[i]]=1+countS.get(s[i],0)
+        countT[t[i]]=1+countT.get(t[i],0)
+    return countS==countT
+        

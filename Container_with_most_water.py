19d1f40b5e8783b0dc05bc53ba84bd7be33a926a
KARNATI SAI VENKAT <90895995+saivenkat735@users.noreply.github.com>
2024-02-29 22:04:07 +0530

    Add files via upload

---
+++ Container_with_most_water.py
@@ -0,0 +1,10 @@
+def maxArea(height):
+    l,r=0,len(height)-1
+    res=0
+    while l<r:
+        res=max(res,min(height(l),height(r))*(r-l))
+        if height[l]<height[r]:
+            l+=1
+        elif height[l]>=height[r]:
+            r-=1
+    return res
